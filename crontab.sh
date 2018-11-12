47453b8d4dc4c4f65e5a400ae8769888d99f60c2
garfunkel <garfunkel@126.com>
2018-11-12 20:11:17 +0800

    crontab

--- crontab.sh
+++ crontab.sh
@@ -3,8 +3,9 @@
 step=2 #间隔的秒数，不能大于60
 
 for (( i = 0; i < 60; i=(i+step) )); do
-    $(/usr/bin/curl http://54.190.29.18/index.php?g=Api&c=Index&a=updateAssign)
+    DATA = $(/usr/bin/curl http://54.190.29.18/index.php?g=Api&c=Index&a=updateAssign)
     sleep $step
+    echo ${DATA} >> run.log
 done
 
 exit 0
