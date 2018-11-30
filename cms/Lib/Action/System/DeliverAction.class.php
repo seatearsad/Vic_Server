bdd23effe94bd7c9b2e1d8f65cad3698ea6504b1
garfunkel <garfunkel@126.com>
2018-11-30 17:48:45 +0800

    deliver fee modify

--- cms/Lib/Action/System/DeliverAction.class.php
+++ cms/Lib/Action/System/DeliverAction.class.php
@@ -716,9 +716,9 @@ class DeliverAction extends BaseAction {
         $b_time = strtotime($b_date);
         $e_time = strtotime($e_date);
 
-        $sql = "SELECT s.order_id, s.create_time,s.uid,s.freight_charge, u.name, u.phone,o.tip_charge,o.price,o.pay_type FROM " . C('DB_PREFIX') . "merchant_store AS m INNER JOIN " . C('DB_PREFIX') . "deliver_supply AS s ON m.store_id=s.store_id LEFT JOIN " . C('DB_PREFIX') . "deliver_user AS u ON s.uid=u.uid LEFT JOIN " . C('DB_PREFIX') . "shop_order AS o ON s.order_id=o.order_id";
+        $sql = "SELECT s.order_id, s.create_time,s.uid,s.freight_charge, u.name, u.phone,o.tip_charge,o.price,o.pay_type,o.coupon_price FROM " . C('DB_PREFIX') . "merchant_store AS m INNER JOIN " . C('DB_PREFIX') . "deliver_supply AS s ON m.store_id=s.store_id LEFT JOIN " . C('DB_PREFIX') . "deliver_user AS u ON s.uid=u.uid LEFT JOIN " . C('DB_PREFIX') . "shop_order AS o ON s.order_id=o.order_id";
 
-        $sql .= ' where s.status = 5 and s.create_time >='.$b_time.' and s.create_time <='.$e_time;
+        $sql .= ' where s.status = 5 and s.create_time >='.$b_time.' and s.create_time <='.$e_time.' and o.is_del = 0';
         $sql .= ' order by s.uid';
 
         $list = D()->query($sql);
@@ -732,7 +732,8 @@ class DeliverAction extends BaseAction {
             $show_list[$v['uid']]['order_num'] = $show_list[$v['uid']]['order_num'] ? $show_list[$v['uid']]['order_num']+ 1 : 1;
             $show_list[$v['uid']]['tip'] = $show_list[$v['uid']]['tip'] ? $show_list[$v['uid']]['tip'] + $v['tip_charge'] : $v['tip_charge'];
             $show_list[$v['uid']]['freight'] = $show_list[$v['uid']]['freight'] ? $show_list[$v['uid']]['freight'] + $v['freight_charge'] : $v['freight_charge'];
-            if($v['pay_type'] != 'moneris'){//统计现金
+            if($v['pay_type'] != 'moneris' && $v['pay_type'] != ''){//统计现金
+                if($v['coupon_price'] > 0) $v['price'] = $v['price'] - $v['coupon_price'];
                 $show_list[$v['uid']]['cash'] = $show_list[$v['uid']]['cash'] ? $show_list[$v['uid']]['cash'] + $v['price'] : $v['price'];
             }
         }
