00d2f643a2a382ad893103ccae615d862e7bcbc1
garfunkel <garfunkel@126.com>
2019-02-01 18:39:13 +0800

    deliver reg bug

--- tpl/Wap/pure/Deliver/step_1.php
+++ tpl/Wap/pure/Deliver/step_1.php
@@ -266,13 +266,13 @@
                     <input type="text" placeholder="Account Holder Name*" id="ahname">
                 </li>
                 <li>
+                    <input type="text" placeholder="Account*(7-12 digit)" id="account">
+                </li>
+                <li>
                     <input type="text" placeholder="Transit(Branch)*(5-digit)" id="transit">
                 </li>
                 <li>
                     <input type="text" placeholder="Institution*(3-digit)" id="institution">
-                </li>
-                <li>
-                    <input type="text" placeholder="Account*(7-12 digit)" id="account">
                 </li>
             </ul>
         </div>
