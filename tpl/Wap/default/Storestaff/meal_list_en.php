b844984fcf1b07d7e76fbd02064169f2f3f2580d
garfunkel <garfunkel@126.com>
2019-03-05 11:20:24 +0800

    merchant debug

--- tpl/Wap/default/Storestaff/meal_list_en.php
+++ tpl/Wap/default/Storestaff/meal_list_en.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-	<title>Merchant center</title>
+	<title>Merchant Center</title>
     <meta name="viewport" content="initial-scale=1, width=device-width, maximum-scale=1, user-scalable=no">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name='apple-touch-fullscreen' content='yes'>
