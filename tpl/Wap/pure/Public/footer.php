041508e0a577abd6a4648478242d932e2142cb71
garfunkel <garfunkel@126.com>
2019-04-30 06:07:19 +0800

    debug 4-30

--- tpl/Wap/pure/Public/footer.php
+++ tpl/Wap/pure/Public/footer.php
@@ -40,7 +40,6 @@
 			</li>
 		</ul>
     </if>
-    <script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async></script>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123655278-1"></script>
     <!-- Facebook Pixel Code -->
     <script>
