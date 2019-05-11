807a378058ca0c5e5ddd377993516b7e9d323eff
garfunkel <garfunkel@126.com>
2019-05-11 15:49:52 +0800

    modify order deliver fee

--- tpl/System/Shop/edit_order.php
+++ tpl/System/Shop/edit_order.php
@@ -59,7 +59,7 @@
 	<if condition="$order['freight_charge'] gt 0">
 	<tr>
 		<th colspan="6">{pigcms{$store['freight_alias']|default='配送费用'}：
-            $<input type="text" name="freight_charge" id="freight_charge" size="5" pattern="[0-9]*" value="{pigcms{$order['freight_charge']|floatval}">
+            $<input type="text" name="freight_charge" id="freight_charge" size="5" value="{pigcms{$order['freight_charge']|floatval}">
         </th>
 	</tr>
 	</if>
