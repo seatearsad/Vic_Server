74570a424eaa8f82ad72b5366c642073d036fd56
garfunkel <garfunkel@126.com>
2019-05-14 11:41:42 +0800

    debug 05.14

--- cms/Lib/Action/Wap/StorestaffAction.class.php
+++ cms/Lib/Action/Wap/StorestaffAction.class.php
@@ -2980,4 +2980,25 @@ class StorestaffAction extends BaseAction
             $this->display();
         }
     }
+
+    protected function get_week($num) {
+        switch ($num) {
+            case 1:
+                return '星期一';
+            case 2:
+                return '星期二';
+            case 3:
+                return '星期三';
+            case 4:
+                return '星期四';
+            case 5:
+                return '星期五';
+            case 6:
+                return '星期六';
+            case 0:
+                return '星期日';
+            default:
+                return '';
+        }
+    }
 }
