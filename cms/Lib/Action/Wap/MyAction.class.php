f2995353e6f30b456a4225bf754f9345faa404f9
garfunkel <garfunkel@126.com>
2019-08-28 17:59:35 +0800

    invitation apk 0828

--- cms/Lib/Action/Wap/MyAction.class.php
+++ cms/Lib/Action/Wap/MyAction.class.php
@@ -5953,13 +5953,13 @@ class MyAction extends BaseAction{
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers. 这里改成smtp.gmail.com
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'caesark882@gmail.com';             // SMTP username 这里改成自己的gmail邮箱，最好新注册一个，因为后期设置会导致安全性降低
-        $mail->Password = 'kkrzakbtivctdtdm';                 // SMTP password 这里改成对应邮箱密码
+        $mail->Username = 'info@tutti.app';             // SMTP username 这里改成自己的gmail邮箱，最好新注册一个，因为后期设置会导致安全性降低
+        $mail->Password = 'Kavl.6668';                 // SMTP password 这里改成对应邮箱密码
         $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 465;
 
 
-        $mail->setFrom('caesark882@gmail.com', 'Caesark');
+        $mail->setFrom('info@tutti.app', 'Tutti');
         $mail->addAddress($address, $address);
 
         $mail->isHTML(true);
