8dd2930471371e457aaa390ee3699507aadc9703
garfunkel <garfunkel@126.com>
2019-08-29 15:55:11 +0800

    new coupon 0829

--- cms/Lib/Action/Wap/MyAction.class.php
+++ cms/Lib/Action/Wap/MyAction.class.php
@@ -5954,12 +5954,12 @@ class MyAction extends BaseAction{
         $mail->Encoding = "base64";
 
         $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp-relay.gmail.com';                       // Specify main and backup SMTP servers. 这里改成smtp.gmail.com
+        $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers. 这里改成smtp.gmail.com
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'info@tutti.app';             // SMTP username 这里改成自己的gmail邮箱，最好新注册一个，因为后期设置会导致安全性降低
-        $mail->Password = 'uxxziisbkmnzprcs';                 // SMTP password 这里改成对应邮箱密码
-        //$mail->Username = 'caesark882@gmail.com';                 // SMTP username 这里改成自己的gmail邮箱，最好新注册一个，因为后期设置会导致安全性降低
-        //$mail->Password = 'kkrzakbtivctdtdm';
+        //$mail->Username = 'info@tutti.app';             // SMTP username 这里改成自己的gmail邮箱，最好新注册一个，因为后期设置会导致安全性降低
+        //$mail->Password = 'uxxziisbkmnzprcs';                 // SMTP password 这里改成对应邮箱密码
+        $mail->Username = 'caesark882@gmail.com';                 // SMTP username 这里改成自己的gmail邮箱，最好新注册一个，因为后期设置会导致安全性降低
+        $mail->Password = 'kkrzakbtivctdtdm';
         $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 465;
 
