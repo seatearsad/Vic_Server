d8ec7c5c1a382fc08db9de94d7079e37f13da8c9
garfunkel <garfunkel@126.com>
2019-11-14 15:14:34 +0800

    debug 11.14

--- cms/Lib/Action/Index/MapAction.class.php
+++ cms/Lib/Action/Index/MapAction.class.php
@@ -11,7 +11,7 @@ class MapAction extends BaseAction{
 		$this->assign('city_name',$now_city['area_name']);
 		//$url = 'http://api.map.baidu.com/place/v2/suggestion?query='.urlencode($_GET['query']).'&region='.urlencode($now_city['area_name']).'&ak=4c1bb2055e24296bbaef36574877b4e2&output=json';
 //		$url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=48.43016873926502,-123.34303379055086&rankby=distance&keyword='.urlencode($_GET['query']).'&key=AIzaSyCKlguA2QFIUVwWTo3danbOqSKv3nYbBCg&language=zh-CN';
-        $url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input='.urlencode($_GET['query']).'&types=address&key=AIzaSyCKlguA2QFIUVwWTo3danbOqSKv3nYbBCg&location=48.43016873926502,-123.34303379055086&radius=50000&components=country:ca&language=en';
+        $url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input='.urlencode($_GET['query']).'&types=address&key=AIzaSyAxHAPoWlRu2Mz8APLwM8Ae6B3x1MJUlvU&location=48.43016873926502,-123.34303379055086&radius=50000&components=country:ca&language=en';
 		import('ORG.Net.Http');
 		$http = new Http();
 		$result = $http->curlGet($url);
@@ -20,7 +20,7 @@ class MapAction extends BaseAction{
 			if($result['status'] == 'OK' && count($result['predictions']) > 0){
 				$return = [];
 				foreach($result['predictions'] as $v) {
-				    $place_url = 'https://maps.googleapis.com/maps/api/place/details/json?placeid='.$v['place_id'].'&key=AIzaSyCKlguA2QFIUVwWTo3danbOqSKv3nYbBCg&fields=geometry&language=en';
+				    $place_url = 'https://maps.googleapis.com/maps/api/place/details/json?placeid='.$v['place_id'].'&key=AIzaSyAxHAPoWlRu2Mz8APLwM8Ae6B3x1MJUlvU&fields=geometry&language=en';
                     $place = $http->curlGet($place_url);
                     $place = json_decode($place,true);
 				    $return[] = [
