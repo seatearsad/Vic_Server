c3264508c54e1b1a1e9e3536c6432fd9873988df
garfunkel <garfunkel@126.com>
2020-03-23 17:24:34 +0800

    debug 0323

--- cms/Lib/Model/Shop_orderModel.class.php
+++ cms/Lib/Model/Shop_orderModel.class.php
@@ -1164,6 +1164,8 @@ class Shop_orderModel extends Model
 	{
 		$where['is_del'] = 0;
 		$order = $this->field(true)->where($where)->find();
+		var_dump($where);
+		var_dump($order);die();
 		if (empty($order)) return false;
 		$order['info'] = D('Shop_order_detail')->field(true)->where(array('order_id' => $order['order_id']))->select();
 		$order['cue_field'] = isset($order['cue_field']) && $order['cue_field'] ? unserialize($order['cue_field']) : '';
