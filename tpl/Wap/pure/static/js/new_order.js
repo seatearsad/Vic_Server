5670e9386ef4ce24c34627d60cce2a111ed5b43d
garfunkel <garfunkel@126.com>
2020-06-15 20:22:29 +0800

    Api debug

--- tpl/Wap/pure/static/js/new_order.js
+++ tpl/Wap/pure/static/js/new_order.js
@@ -18,7 +18,7 @@ function getNewOrder(){
     $.post(new_url,{last_time:last_time},function(result){
         if(result.error == 0){
             last_time = result.new_time;
-            if(result.is_new == 0){
+            if(result.is_new == 1){
                 layer.open({
                     title:[' ','border:none'],
                     content:tip_message,
