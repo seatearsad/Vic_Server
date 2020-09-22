67ee34edc4a7fd8769ee364a1c9ab012d56e14ad
garfunkel <garfunkel@126.com>
2020-09-22 16:30:40 +0800

    0922 debug

--- cms/Lib/Action/Wap/ShopAction.class.php
+++ cms/Lib/Action/Wap/ShopAction.class.php
@@ -3273,6 +3273,7 @@ class ShopAction extends BaseAction{
 	public function status()
 	{
 		$order_id = isset($_GET['order_id']) ? intval($_GET['order_id']) : 0;
+		var_dump(array('order_id' => $order_id, 'uid' => $this->user_session['uid']));die();
 		if ($order = D('Shop_order')->get_order_detail(array('order_id' => $order_id, 'uid' => $this->user_session['uid']))) {
         //if ($order = D('Shop_order')->get_order_detail(array('order_id' => $order_id))) {
 			$storeName = D("Merchant_store")->field('`name`, `phone`')->where(array('store_id' => $order['store_id']))->find();
