79d153220ffb3f856ec82a05e96a3dbd21afe1e1
garfunkel <garfunkel@126.com>
2020-11-19 19:30:17 +0800

    sms debug

--- cms/Lib/Action/Wap/MyAction.class.php
+++ cms/Lib/Action/Wap/MyAction.class.php
@@ -6039,14 +6039,13 @@ class MyAction extends BaseAction{
         //$mail->Host = 'smtp.gmail.com';
         $mail->Host = 'smtp-relay.gmail.com';                  // Specify main and backup SMTP servers. 这里改成smtp.gmail.com
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'info@tutti.app';                    // SMTP username 这里改成自己的gmail邮箱，最好新注册一个，因为后期设置会导致安全性降低
+        $mail->Username = 'donotreply.tutti@gmail.com';                    // SMTP username 这里改成自己的gmail邮箱，最好新注册一个，因为后期设置会导致安全性降低
         $mail->Password = $password;                 // SMTP password 这里改成对应邮箱密码
         $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 465;
 
 
-        $mail->setFrom('info@tutti.app', 'Tutti');
-        //$mail->setFrom('caesark882@gmail.com', 'Caesark');
+        $mail->setFrom('donotreply.tutti@gmail.com', 'Tutti');
         $mail->addAddress($address, $address);
 
         $mail->isHTML(true);
