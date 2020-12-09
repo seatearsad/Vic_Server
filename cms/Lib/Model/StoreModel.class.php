30fe4fc307efb5bcb4c8bd43bc24ae32a9b8d8fb
garfunkel <garfunkel@126.com>
2020-12-09 15:04:08 +0800

    newui api

--- cms/Lib/Model/StoreModel.class.php
+++ cms/Lib/Model/StoreModel.class.php
@@ -990,18 +990,21 @@ class StoreModel extends Model
             $delivery = D('Deliver_supply')->where(array('order_id'=>$order['order_id']))->find();
             $now_time = time();
             $check_time = $delivery['create_time'] + $delivery['dining_time']*60;
-            if($add_time > 0){
-                $add_log = D('Shop_order_log')->field(true)->where(array('order_id' => $order['order_id'],'status'=>33))->order('id DESC')->select();
+            if($add_time > 0) {
+                $add_log = D('Shop_order_log')->field(true)->where(array('order_id' => $order['order_id'], 'status' => 33))->order('id DESC')->select();
                 $add_time = 0;
-                foreach ($add_log as $v){
+                foreach ($add_log as $v) {
                     $add_time += $v['note'];
                 }
-                $desc = "The restaurant needs another ".$add_time." min to prepare your order, so your order is expected to be ready by ". date("H:i", $check_time);;
-            }else {
-                if ($now_time < $check_time)
+            }
+
+            if($now_time < $check_time){
+                if ($add_time == 0)
                     $desc = "Your order is expected to be ready by " . date("H:i", $check_time);
                 else
-                    $desc = "Your order is ready and will be picked up shortly.";
+                    $desc = "The restaurant needs another ".$add_time." min to prepare your order, so your order is expected to be ready by ". date("H:i", $check_time);;
+            }else {
+                $desc = "Your order is ready and will be picked up shortly.";
             }
         }
 
