9a50b155450d2766a32f20be1a214ba979037989
garfunkel <garfunkel@126.com>
2020-12-24 15:44:01 +0800

    backend debug

--- tpl/Wap/pure/Deliver/step_4.php
+++ tpl/Wap/pure/Deliver/step_4.php
@@ -140,5 +140,18 @@
 <script>
     $("body").css({"height":$(window).height()});
 </script>
+<!-- Begin INDEED conversion code -->
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var indeed_conversion_id = '3862015109939163';
+    var indeed_conversion_label = '';
+    /* ]]> */
+</script>
+<script type="text/javascript" src="https://conv.indeed.com/applyconversion.js">
+</script>
+<noscript>
+    <img height=1 width=1 border=0 src="https://conv.indeed.com/pagead/conv/3862015109939163/?script=0">
+</noscript>
+<!-- End INDEED conversion code -->
 </body>
 </html>
