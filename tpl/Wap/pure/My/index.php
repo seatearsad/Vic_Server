cb46b3db0a5669f65eeeb52c8eb617bed002c056
303172979@qq.com <303172979@qq.com>
2020-12-31 19:08:12 +0800

    个人中文首页

--- tpl/Wap/pure/My/index.php
+++ tpl/Wap/pure/My/index.php
@@ -1,33 +1,37 @@
-<?php if(!defined('PigCms_VERSION')){ exit('deny access!');} ?>
+<?php if (!defined('PigCms_VERSION')) {
+    exit('deny access!');
+} ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<title>{pigcms{:L('_B_PURE_MY_33_')}</title>
-		<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, width=device-width,viewport-fit=cover"/>
-		<meta name="apple-mobile-web-app-capable" content="yes"/>
-		<meta name='apple-touch-fullscreen' content='yes'/>
-		<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
-		<meta name="format-detection" content="telephone=no"/>
-		<meta name="format-detection" content="address=no"/>
-		<link rel="stylesheet" type="text/css" href="{pigcms{$static_path}css/common.css?215"/>
-		<script type="text/javascript" src="{pigcms{:C('JQUERY_FILE_190')}" charset="utf-8"></script>
-		<script type="text/javascript" src="{pigcms{$static_path}js/iscroll.js?444" charset="utf-8"></script>
-		<script type="text/javascript" src="{pigcms{$static_path}js/idangerous.swiper.min.js" charset="utf-8"></script>
-		<script type="text/javascript" src="{pigcms{$static_path}js/fastclick.js" charset="utf-8"></script>
-		<script type="text/javascript" src="{pigcms{$static_path}layer/layer.m.js" charset="utf-8"></script>
-		<!--script type="text/javascript" src="{pigcms{$static_path}js/common.js?211" charset="utf-8"></script-->
+<head>
+    <meta charset="utf-8"/>
+    <title>{pigcms{:L('_B_PURE_MY_33_')}</title>
+    <meta name="viewport"
+          content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, width=device-width,viewport-fit=cover"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name='apple-touch-fullscreen' content='yes'/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="format-detection" content="address=no"/>
+    <link rel="stylesheet" type="text/css" href="{pigcms{$static_path}css/common.css?215"/>
+    <script type="text/javascript" src="{pigcms{:C('JQUERY_FILE_190')}" charset="utf-8"></script>
+    <script type="text/javascript" src="{pigcms{$static_path}js/iscroll.js?444" charset="utf-8"></script>
+    <script type="text/javascript" src="{pigcms{$static_path}js/idangerous.swiper.min.js" charset="utf-8"></script>
+    <script type="text/javascript" src="{pigcms{$static_path}js/fastclick.js" charset="utf-8"></script>
+    <script type="text/javascript" src="{pigcms{$static_path}layer/layer.m.js" charset="utf-8"></script>
+    <!--script type="text/javascript" src="{pigcms{$static_path}js/common.js?211" charset="utf-8"></script-->
 
-		<link href="{pigcms{$static_path}css/check.css" rel="stylesheet"/>
+    <link href="{pigcms{$static_path}css/check.css" rel="stylesheet"/>
     <style>
-        .main{
+        .main {
             width: 100%;
             padding-top: 60px;
             max-width: 640px;
             min-width: 320px;
             margin: 0 auto;
         }
-        .user_avatar{
+
+        .user_avatar {
             width: 90px;
             height: 90px;
             margin: 20px auto 0 auto;
@@ -36,47 +40,55 @@
             border-radius: 45px;
             background-size: cover;
         }
-        .user_avatar_default{
+
+        .user_avatar_default {
             width: 100px;
             height: 100px;
             margin: 20px auto 0 auto;
             background-size: cover;
             background-image: url("./tpl/Static/blue/images/wap/user.png");
         }
-        .user_name{
+
+        .user_name {
             text-align: center;
             font-size: 1.2em;
             margin-top: 10px;
         }
-        .gray_line{
+
+        .gray_line {
             width: 100%;
             height: 2px;
             margin-top: 15px;
             background-color: #cccccc;
         }
-        .gray_k{
+
+        .gray_k {
             width: 10%;
             height: 2px;
             background-color: #f4f4f4;
             margin: -2px auto 0 auto;
         }
-        .main ul{
+
+        .main ul {
             margin: 20px 0 0;
             width: 100%;
         }
-        .main ul li{
+
+        .main ul li {
             width: 90%;
             height: 50px;
             margin-left: 5%;
             background-color: white;
+            border-radius: 10px;
             list-style: none;
             margin-bottom: 10px;
-            background-image: url("./tpl/Static/blue/images/new/black_arrow.png");
-            background-size: auto 16px;
+            background-image: url("./tpl/Static/blue/images/new/icon_right_arrow.png");
+            background-size: auto 20px;
             background-repeat: no-repeat;
-            background-position:right 10px center;
+            background-position: right 10px center;
         }
-        .main ul li div{
+
+        .main ul li div {
             line-height: 50px;
             font-size: 1.4em;
             padding-left: 60px;
@@ -84,131 +96,175 @@
             background-repeat: no-repeat;
             background-position: 10px center;
         }
-        .main ul a:nth-child(1) li div{
+
+        .main ul a:nth-child(1) li div {
             background-image: url("./tpl/Static/blue/images/wap/profile.png");
         }
-        .main ul a:nth-child(2) li div{
-            background-image: url("./tpl/Static/blue/images/wap/balance.png");
-        }
-        .main ul a:nth-child(3) li div{
+
+        /*.main ul a:nth-child(2) li div {*/
+            /*background-image: url("./tpl/Static/blue/images/wap/balance.png");*/
+        /*}*/
+
+        .main ul a:nth-child(2) li div {
             background-image: url("./tpl/Static/blue/images/wap/coupon.png");
         }
-        .main ul a:nth-child(4) li div{
-            background-image: url("./tpl/Static/blue/images/wap/orders.png");
-        }
-        .main ul a:nth-child(5) li div{
+
+        /*.main ul a:nth-child(4) li div {*/
+            /*background-image: url("./tpl/Static/blue/images/wap/orders.png");*/
+        /*}*/
+
+        .main ul a:nth-child(3) li div {
             background-image: url("./tpl/Static/blue/images/wap/language.png");
         }
 
-        #event_div{
+        #event_div {
             width: 90%;
-            height: 80px;
             margin-left: 5%;
-            background-color: white;
+            background-color: rgba(255, 165, 44, 0.21);
             margin-top: 10px;
             border-radius: 10px;
+            border-style:solid;
+            border-width:2px;
+            border-color:#ffa52d;
             padding: 10px 2% 10px 70px;
             background-image: url("./tpl/Static/blue/images/new/gift_icon.png");
             background-repeat: no-repeat;
             background-size: 55px auto;
             background-position: 10px 10px;
         }
-        #event_name{
+
+        #event_name {
             font-weight: bold;
-            font-size: 18px;
+            font-size: 19px;
+            color:#ffa52d;
         }
-	</style>
-        <include file="Public:facebook"/>
+        #event_desc {
+            font-size: 14px;
+        }
+        #courier_div {
+            width: 90%;
+            margin-left: 5%;
+            background-color: rgba(1, 98, 255, 0.2);
+            margin-top: 20px;
+            border-radius: 10px;
+            border-style:solid;
+            border-width:2px;
+            border-color: #013cff;
+            padding: 10px 2% 10px 70px;
+            background-image: url("./tpl/Static/blue/images/new/icon_deliver.png");
+            background-repeat: no-repeat;
+            background-size: 55px auto;
+            background-position: 10px 10px;
+        }
+
+        #courier_name {
+            font-weight: bold;
+            font-size: 19px;
+            color: #013cff;
+        }
+        #courier_desc {
+            font-size: 14px;
+        }
+    </style>
+    <include file="Public:facebook"/>
 </head>
 <body>
-    <include file="Public:header"/>
-	<div class="main">
-        <if condition="$now_user['avatar']">
-            <div class="user_avatar" style='background-image: url("{pigcms{$now_user['avatar']}")'></div>
-        <else />
-            <div class="user_avatar_default"></div>
-        </if>
-        <div class="user_name">{pigcms{$now_user['nickname']}</div>
-        <div class="gray_line"></div>
-        <div class="gray_k"></div>
-        <if condition="$event">
-            <a href="{pigcms{:U('My/invitation')}">
-                <div id="event_div">
-                    <div id="event_name">{pigcms{$event.name}</div>
-                    <div id="event_desc">{pigcms{$event.desc}</div>
-                </div>
-            </a>
-        </if>
-        <ul>
-            <a href="{pigcms{:U('My/myinfo')}">
+<include file="Public:header"/>
+<div class="main">
+    <div>{pigcms{:replace_lang_str(L('_ND_HI_'),$now_user['nickname'])}
+    </div>
+    <div>{pigcms{$coupon_number}{pigcms{:L('QW_V2_COUPONS')}, ${pigcms{$now_user.now_money_two}{pigcms{:L('QW_V2_CREDITS')}
+    </div>
+    <div>
+        <button type="button">{pigcms{:L('_RECHARGE_TXT_')}</button>
+    </div>
+
+    <div class="gray_k"></div>
+    <if condition="$event">
+        <a href="{pigcms{:U('My/invitation')}">
+            <div id="event_div">
+                <div id="event_name">{pigcms{$event.name}</div>
+                <div id="event_desc">{pigcms{$event.desc}</div>
+            </div>
+        </a>
+    </if>
+    <ul>
+        <a href="{pigcms{:U('My/myinfo')}">
             <li>
                 <div>Profile</div>
             </li>
-            </a>
-            <a href="{pigcms{:U('My/my_money')}">
-            <li>
-                <div>Balance</div>
-            </li>
-            </a>
-            <a href="{pigcms{:U('My/coupon')}">
+        </a>
+<!--        <a href="{pigcms{:U('My/my_money')}">-->
+<!--            <li>-->
+<!--                <div>Balance</div>-->
+<!--            </li>-->
+<!--        </a>-->
+        <a href="{pigcms{:U('My/coupon')}">
             <li>
                 <div>Coupon</div>
             </li>
-            </a>
-            <a href="{pigcms{:U('My/shop_order_list')}">
-            <li>
-                <div>Orders</div>
-            </li>
-            </a>
-            <a href="{pigcms{:U('My/language')}">
+        </a>
+<!--        <a href="{pigcms{:U('My/shop_order_list')}">-->
+<!--            <li>-->
+<!--                <div>Orders</div>-->
+<!--            </li>-->
+<!--        </a>-->
+        <a href="{pigcms{:U('My/language')}">
             <li>
                 <div>Language</div>
             </li>
-            </a>
-        </ul>
-    </div>
-	<include file="Public:footer"/>
-    <script type="text/javascript">
-        window.zESettings = {
-            webWidget: {
-                color: {
-                    launcher: '#ffa52d', // This will also update the badge
-                    launcherText: '#ffffff',
-                },
+        </a>
+    </ul>
+    <a href="https://www.tutti.app/wap.php?g=Wap&c=Index&a=courier">
+        <div id="courier_div">
+            <div id="courier_name">{pigcms{:L('QW_V2_COURIER_TITLE')}</div>
+            <div id="courier_desc">{pigcms{:L('QW_V2_COURIER_DESC')}</div>
+        </div>
+    </a>
+</div>
+<include file="Public:footer"/>
+<script type="text/javascript">
+    window.zESettings = {
+        webWidget: {
+            color: {
+                launcher: '#ffa52d', // This will also update the badge
+                launcherText: '#ffffff',
+            },
+            mobile: {
+                labelVisible: true
+            },
+            offset: {
                 mobile: {
-                    labelVisible: true
-                },
-                offset: {
-                    mobile: {
-                        horizontal: '-10px',
-                        vertical: '35px'
-                    }
+                    horizontal: '-10px',
+                    vertical: '35px'
                 }
             }
-        };
-    </script>
-    <!-- Start of tuttidelivery Zendesk Widget script -->
-    <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=fe2c146c-36c1-4a86-807d-0ebeaa3d0a58"> </script>
-    <!-- End of tuttidelivery Zendesk Widget script -->
+        }
+    };
+</script>
+<!-- Start of tuttidelivery Zendesk Widget script -->
+<script id="ze-snippet"
+        src="https://static.zdassets.com/ekr/snippet.js?key=fe2c146c-36c1-4a86-807d-0ebeaa3d0a58"></script>
+<!-- End of tuttidelivery Zendesk Widget script -->
 
-    <script type="text/javascript">
-        zE('webWidget', 'identify', {
-            name: "{pigcms{$now_user['nickname']}",
-            email:"{pigcms{$now_user['email']}",
-            phone:"{pigcms{$now_user['phone']}",
-            organization: "User_{pigcms{$now_user['uid']}"
-        });
-        zE('webWidget', 'prefill', {
-            name: {
-                value: "{pigcms{$now_user['nickname']}"
-            },
-            email: {
-                value: "{pigcms{$now_user['email']}"
-            },
-            phone: {
-                value: "{pigcms{$now_user['phone']}"
-            }
-        });
-    </script>
+<script type="text/javascript">
+    zE('webWidget', 'identify', {
+        name: "{pigcms{$now_user['nickname']}",
+        email: "{pigcms{$now_user['email']}",
+        phone: "{pigcms{$now_user['phone']}",
+        organization: "User_{pigcms{$now_user['uid']}"
+    });
+    zE('webWidget', 'prefill', {
+        name: {
+            value: "{pigcms{$now_user['nickname']}"
+        },
+        email: {
+            value: "{pigcms{$now_user['email']}"
+        },
+        phone: {
+            value: "{pigcms{$now_user['phone']}"
+        }
+    });
+</script>
 </body>
 </html>
