c819557f01594a216d2633e886ab9b1633fe67dc
garfunkel <garfunkel@126.com>
2021-03-26 14:35:15 +0800

    backend language

--- tpl/System/Index/profile.php
+++ tpl/System/Index/profile.php
@@ -7,29 +7,30 @@
 			<input type="hidden" class="input-text" name="system_menu" value=""/>
 			<table cellpadding="0" cellspacing="0" class="table_form" width="100%">
 				<tr>
-					<th  width="120">帐号：</td>
+					<th  width="120">{pigcms{:L('B_USERNAME')}：</td>
 					<td>{pigcms{$admin.account}</th>
 				</tr>
 				<tr>
-					<th  width="120">真实姓名：</th>
+					<th  width="120">{pigcms{:L('B_FULLNAME')}：</th>
 					<td><input type="text" class="input-text"  name="realname" value="{pigcms{$admin.realname}" validate="required:true" /></td>
 				</tr>
 				<tr>
-					<th>邮箱：</th>
+					<th>{pigcms{:L('B_EMAIL')}：</th>
 					<td><input type="text" class="input-text"  name="email" value="{pigcms{$admin.email}" validate="required:true,email:true,minlength:1,maxlength:40" /></td>
 				</tr>
-				<tr>
+				<!--tr>
 					<th>Q Q：</th>
 					<td><input type="text" class="input-text"  name="qq" value="{pigcms{$admin.qq}" validate="required:true,qq:true" /></td>
-				</tr>
+				</tr-->
+                <input type="hidden" class="input-text"  name="qq" value="{pigcms{$admin.qq|default='123'}" />
 				<tr>
-					<th>手机号码：</th>
+					<th>{pigcms{:L('B_PHONE')}：</th>
 					<td><input type="text" class="input-text"  name="phone" value="{pigcms{$admin.phone}"  validate="required:true,mobile:true" /></td>
 				</tr>
 				<tr>
-					<th>菜单排序：</th>
+					<th>{pigcms{:L('B_MENUORDER')}：</th>
 					<td>
-						描述：此功能只对当前用户生效。数字越大，排序越靠前。<br/>
+						{pigcms{:L('B_MODES')}<br/>
 						<volist name="system_menu" id="vv" key="k">
 							<div style="margin-top:10px;width:30%;float:left;">{pigcms{$vv['name']} <input type="number" min="0" class="input-text input1" name="{pigcms{$vv.id}" value="{pigcms{$sort_menus_son[$vv['id']]}" /></div>
 							<if condition="$k%3 eq 0"><br /></if>
@@ -38,8 +39,8 @@
 				</tr>
 			</table>
 			<div class="btn">
-				<input TYPE="submit" id="submit" name="dosubmit" value="提交" class="button" />
-				<input type="reset" value="取消" class="button" />
+				<input TYPE="submit" id="submit" name="dosubmit" value="{pigcms{:L('BASE_SUBMIT')}" class="button" />
+				<input type="reset" value="{pigcms{:L('BASE_CANCEL')}" class="button" />
 			</div>
 		</form>
 	</div>
