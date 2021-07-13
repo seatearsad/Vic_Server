9a97f44a0e021127ff21771664946e0710e99a0a
garfunkel <garfunkel@126.com>
2021-07-13 17:03:11 +0800

    backend debug

--- cms/Lib/Model/System_couponModel.class.php
+++ cms/Lib/Model/System_couponModel.class.php
@@ -189,7 +189,7 @@ class System_couponModel extends Model{
         $where['h.is_use'] = array('neq',1);  //状态正常
 
         $where['uid'] = $uid;
-        $where['_string'] = "(c.cate_name='".$table."') OR (c.cate_name ='all')";
+        $where['_string'] = "(c.cate_name='".$table."') OR (c.cate_name ='all') OR (c.cate_name = '0')";
         $res = M('System_coupon_hadpull')->join('as h left join '.C('DB_PREFIX').'system_coupon c ON h.coupon_id=c.coupon_id')->field('h.id,c.coupon_id,c.name,c.order_money,c.discount,h.phone,h.receive_time,c.platform,c.cate_name,c.cate_id,c.start_time,c.end_time,h.is_use ,c.status,c.qrcode_id,c.des,c.des_detial,c.img,c.allow_new')->order('c.order_money Asc, h.is_use ASC ,c.discount DESC,c.add_time DESC')->where($where)->select();
 
         foreach($res as $key=>&$v){
