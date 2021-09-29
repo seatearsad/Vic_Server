f0724dbbb0aa02d5dbf7bf5965bf22c190828e51
garfunkel <garfunkel@126.com>
2021-09-29 17:58:21 +0800

    deliverect api

--- cms/Lib/Action/Api/DeliverectAction.class.php
+++ cms/Lib/Action/Api/DeliverectAction.class.php
@@ -8,19 +8,22 @@
 
 class DeliverectAction
 {
+    private $site_url;
     public function __construct()
     {
         file_put_contents("./deliverect_log.log",date("Y/m/d")."   ".date("h:i:sa")."   "."Deliverect" ."   ". $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'--'.json_encode($GLOBALS['HTTP_RAW_POST_DATA'])."\r\n",FILE_APPEND);
+        $config = D('Config')->where(array('name'=>'site_url'))->find();
+        $this->site_url = $config['value'];
     }
 
     public function channelStatus(){
         //echo "channelStatus";
         $link_array = array(
-            "statusUpdateURL"=> C('config.site_url')."/deliverect/statusUpdate",
-            "menuUpdateURL"=> C('config.site_url')."/deliverect/menuUpdate",
-            "snoozeUnsnoozeURL"=> C('config.site_url')."/deliverect/snoozeUnsnooze",
-            "busyModeURL"=> C('config.site_url')."/deliverect/busyMode",
-            "updatePrepTimeURL"=> C('config.site_url')."/deliverect/updatePrepTimeURL"
+            "statusUpdateURL"=> $this->site_url."/deliverect/statusUpdate",
+            "menuUpdateURL"=> $this->site_url."/deliverect/menuUpdate",
+            "snoozeUnsnoozeURL"=> $this->site_url."/deliverect/snoozeUnsnooze",
+            "busyModeURL"=> $this->site_url."/deliverect/busyMode",
+            "updatePrepTimeURL"=> $this->site_url."/deliverect/updatePrepTimeURL"
         );
 
         echo json_encode($link_array);
