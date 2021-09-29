f5fe5fcb5cf25d3fabfa2e33ffac5e9568c8e794
garfunkel <garfunkel@126.com>
2021-09-29 15:07:01 +0800

    deliverect api

--- cms/Lib/Action/Api/DeliverectAction.class.php
+++ cms/Lib/Action/Api/DeliverectAction.class.php
@@ -10,11 +10,20 @@ class DeliverectAction
 {
     public function __construct()
     {
-        file_put_contents("./deliverect_log.log",date("Y/m/d")."   ".date("h:i:sa")."   "."Deliverect" ."   ". $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'--'.json_encode($_POST)."\r\n",FILE_APPEND);
+        file_put_contents("./deliverect_log.log",date("Y/m/d")."   ".date("h:i:sa")."   "."Deliverect" ."   ". $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'--'.json_encode($_REQUEST)."\r\n",FILE_APPEND);
     }
 
     public function channelStatus(){
-        echo "channelStatus";
+        //echo "channelStatus";
+        $link_array = array(
+            "statusUpdateURL"=> "https://integrator.com/statusUpdate",
+            "menuUpdateURL"=> "https://integrator.com/menuUpdate",
+            "snoozeUnsnoozeURL"=> "https://integrator.com/snoozeUnsnooze",
+            "busyModeURL"=> "https://integrator.com/busyMode",
+            "updatePrepTimeURL"=> "https://integrator.com/updatePrepTimeURL"
+        );
+
+        echo json_encode($link_array);
     }
 
     public function snooze(){
