5a8f99d4520f31cf1b2a1831c926c2069da09406
garfunkel <garfunkel@126.com>
2021-09-30 16:29:04 +0800

    deliverect api

--- cms/Lib/Action/Api/DeliverectAction.class.php
+++ cms/Lib/Action/Api/DeliverectAction.class.php
@@ -9,14 +9,15 @@
 class DeliverectAction
 {
     private $site_url;
+    private $link_type = 1;
     public function __construct()
     {
         file_put_contents("./deliverect_log.log",date("Y/m/d")."   ".date("h:i:sa")."   "."Deliverect" ."   ". $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'--'.json_encode(file_get_contents("php://input"))."\r\n",FILE_APPEND);
-        $config = D('Config')->where(array('name'=>'site_url'))->find();
-        $this->site_url = $config['value'];
     }
 
     public function channelStatus(){
+        $config = D('Config')->where(array('name'=>'site_url'))->find();
+        $this->site_url = $config['value'];
         //echo "channelStatus";
         $link_array = array(
             "statusUpdateURL"=> $this->site_url."/deliverect/statusUpdate",
@@ -25,6 +26,33 @@ class DeliverectAction
             "busyModeURL"=> $this->site_url."/deliverect/busyMode",
             "updatePrepTimeURL"=> $this->site_url."/deliverect/updatePrepTimeURL"
         );
+
+        $data = json_decode(file_get_contents("php://input"),true);
+        $store_id = $data['channelLocationId'];
+        $link_id = $data['channelLinkId'];
+        $status = $data['status'];
+
+        $link_status = 0;
+        switch ($status){
+            case 'register':
+                $link_status = 1;
+                break;
+            case 'active':
+                $link_status = 2;
+                break;
+            case 'inactive':
+                $link_status = 3;
+                break;
+
+            default:
+                break;
+        }
+
+        $updateData['link_type'] = $this->link_type;
+        $updateData['link_id'] = $link_id;
+        $updateData['link_status'] =$link_status;
+
+        D('Merchant_store')->where(array('store_id'=>$store_id))->save($updateData);
 
         echo json_encode($link_array);
     }
