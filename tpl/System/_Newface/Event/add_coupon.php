9c412c2ee303a69d231f746957e425f7f7452c4d
garfunkel <garfunkel@126.com>
2022-02-16 19:41:52 +0800

    new store discount 22.02.16

--- tpl/System/_Newface/Event/add_coupon.php
+++ tpl/System/_Newface/Event/add_coupon.php
@@ -44,7 +44,7 @@
                                 <div class="col-sm-9">
                                     <input type="text" class="form-control" name="discount" size="20" validate="maxlength:20,required:true" value="{pigcms{$coupon.discount|default=''}"/>
                                     <if condition="$event_type eq 6">
-                                        eg.Key in 0.80 for a 20% off discount
+                                        eg. Key in 0.80 for a 20% off discount
                                     </if>
                                 </div>
                             </div>
