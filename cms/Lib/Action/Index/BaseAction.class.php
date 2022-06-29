614817aa0f77f6dee21b7d85d07942396122cada
garfunkel <garfunkel@126.com>
2022-06-29 14:47:29 +0800

    debug 22.06.29

--- cms/Lib/Action/Index/BaseAction.class.php
+++ cms/Lib/Action/Index/BaseAction.class.php
@@ -6,10 +6,7 @@
 class BaseAction extends CommonAction{
     protected function _initialize(){
 		parent::_initialize();
-
-		if(!function_exists('indexfdksajflkjsadmbvlknasdfa')){
-			redirect('http://www.pigcms.com');
-		}
+		
 		if($this->config['show_scroll_msg']){
 			$scroll_msg = D('Scroll_msg')->get_msg();
 			$this->assign('scroll_msg',$scroll_msg);
